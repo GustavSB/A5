fc8643f170042ff6b926314c9ac9d066c2d79fa4
GustavSB <Gustav.strengen@gmail.com>
2019-10-29 10:58:24 +0100

    Changes to App
    made it a true main

--- src/main/java/no/rulingu/App.java
+++ src/main/java/no/rulingu/App.java
@@ -15,7 +15,13 @@ public class App
 {
     public static void main( String[] args )
     {
+        App app = new App ();
+        app.go();
+    }
+
+    public void go(){
         POST post = new POST("datakomm.work", 80);
+        GetTask getTask = new GetTask("dataKOMM.WORK", 80);
         post.sendauhorizationinformation();
     }
 }
