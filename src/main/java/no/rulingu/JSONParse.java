8a370719954bc3ebc3c2727eb881f7f26e05afa6
GustavSB <Gustav.strengen@gmail.com>
2019-10-29 11:40:52 +0100

    added parse

---
+++ src/main/java/no/rulingu/JSONParse.java
@@ -0,0 +1,18 @@
+package no.rulingu;
+import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class JSONParse {/*
+
+    JSONObject obj = new JSONObject(" .... ");
+    String pageName = obj.getJSONObject("pageInfo").getString("pageName");
+
+    JSONArray arr = obj.getJSONArray("posts");
+        for (int i = 0; i < arr.length(); i++)
+        {
+            String post_id = arr.getJSONObject(i).getString("post_id");
+        }*/
+}
